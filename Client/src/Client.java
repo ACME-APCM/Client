630556e15fee1d1ce50e6d3b9df1c2c9468d333e
Carlin Nicola <nicola.carlin-1@studenti.unitn.it>
2023-12-28 16:09:07 +0100

    First GUI implementation

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,16 +1,134 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 /**
  * This class implements java socket client
- * @author pankaj
+ * @author ACMEteam
  *
  */
-public class Client {
+
+public class Client extends JFrame implements ActionListener {
+    // label that gives feedback on the action performed
+    JLabel feedbackLabel;
+    // Text area
+    JTextArea chatArea;
+    // file
+    String file;
+
+    // constructor that initializes class variables
+	public Client() {
+        feedbackLabel = new JLabel("ready");
+        chatArea = new JTextArea(30, 120);
+    }
+
+    //read string from file
+    public String readFile(String path) {
+        // if the path is not set, ask the user via an interactive window
+        if (path.equals("")){
+            // create an interactive file chooser window
+            JFileChooser chooser = new JFileChooser();
+            // open in current directory
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDirectory);
+
+            // show the open dialog
+            int r = chooser.showOpenDialog(null);
+
+            // if the user selects a file
+            if (r == JFileChooser.APPROVE_OPTION) {
+                // save the path of the selected file
+                path = (chooser.getSelectedFile().getAbsolutePath());
+            } else {
+                // if no selection end process with empty output
+                return "";
+            }
+        }
+        // open file and read content
+        try (
+            FileInputStream fis = new FileInputStream(new File(path));
+        ) {
+            byte data[] = new byte[fis.available()];
+            fis.read(data);
+            fis.close();
+            String str = new String(data);
+            return str;
+        } catch (IOException ex) {
+            // in case of reading error print error on console and return an empty output
+            System.out.println(ex);
+            return "";
+        }
+    }
+    
+    // set up main window
+	public static void main(String args[]) {
+		
+		// I/O buttons
+		JButton saveButton = new JButton("save");
+		JButton openButton = new JButton("open");
+
+		// make an object that handles the events
+		Client ct = new Client();
+
+		// add action listener to the button to capture user
+		// response on buttons
+		openButton.addActionListener(ct);
+		saveButton.addActionListener(ct);
+         
+        JScrollPane chatScroll = new JScrollPane(ct.chatArea);
+        JPanel chatPanel = new JPanel(new BorderLayout());
+        chatPanel.add(new JLabel("Text:", SwingConstants.LEFT), BorderLayout.PAGE_START);
+        chatPanel.add(chatScroll);
+       
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.LINE_AXIS));
+        inputPanel.add(openButton);
+        inputPanel.add(saveButton);
+
+        JPanel feedbackPanel = new JPanel();
+        feedbackPanel.add(ct.feedbackLabel);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+        mainPanel.add(chatPanel);
+        mainPanel.add(Box.createVerticalStrut(10));
+        mainPanel.add(inputPanel);
+        mainPanel.add(feedbackPanel);
+
+        JFrame frame = new JFrame("ACME");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(mainPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+	}
+
+    // manage button clicks
+	public void actionPerformed(ActionEvent evt) {
+		String com = evt.getActionCommand();
+        switch (com) {
+            // open button
+            case "open":
+                // ask user to select file to read and read it
+                file = readFile("");
+                // give feedback
+                chatArea.setText(file.toString());
+                feedbackLabel.setText("File Opened");
+                break;
+            // save button
+            case "save":
+                feedbackLabel.setText(chatArea.getText());
+                file = chatArea.getText(); //Text updated
+                break;
+            default:
+                break;
+        }
+	}
+}
+
+//Code for socket
+/*public class Client {
 
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
         //get the localhost IP address, if server is running on some other IP, you need to use that
@@ -36,4 +154,4 @@ public class Client {
             Thread.sleep(100);
         }
     }
-}
+}*/
